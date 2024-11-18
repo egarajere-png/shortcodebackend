c9ae93dd14736c7437013b45ad08807a48b3fec5
Samuel Waithaka <samuel.waithaka@abcthebank.com>
2024-11-18 10:02:48 +0300

    Volume short codes

--- src/main/java/com/abcbank/shortcode/shortcode/middleware/ShortCodeService.java
+++ src/main/java/com/abcbank/shortcode/shortcode/middleware/ShortCodeService.java
@@ -21,12 +21,12 @@ public class ShortCodeService {
 	
 	public void sendReceiptEmail(ShortCode shortCode) {
 		
-		log.info("\nSending slip for eslip {}", shortCode.getShortCode());
+		log.info("Sending slip for eslip {}", shortCode.getShortCode());
 
 		String emailAddress = shortCode.getEmailAddress();
 		String customerName = shortCode.getAccountName();
 		
-		log.info("\n\nAbout to emailAddress: " + emailAddress);
+		log.info("About to emailAddress: " + emailAddress);
 		String from = "ABC Bank Support<talk2us@abcthebank.com>";
 		String subject = "ABC Bank - New Short-code " + shortCode.getShortCode();
 		String body = "Dear " + customerName + ",\n\n" + 
@@ -34,7 +34,7 @@ public class ShortCodeService {
 					"\nYou can share it so others can send you money with this short-code as the account for Mpesa paybill." +
 					"\n\nABC Bank Team";
 		emailer.send(from, emailAddress.toLowerCase(), "", subject, body, "/tmp/" + shortCode.getShortCode() + ".pdf");
-		log.info("\n\nReceipt has been sent on email");
+		log.info("Receipt has been sent on email");
 	}
 	
 	public boolean validateRequest(ShortCode shortCode) {
